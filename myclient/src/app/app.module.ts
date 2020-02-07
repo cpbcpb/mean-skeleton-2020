6242405344dac25ab6ee170d62a2d3626e98bbd6
Christine Beck <39750121+cpbcpb@users.noreply.github.com>
2020-02-07 16:50:46 -0500

    Created auth as an example service and added import statement for auth to app.module.ts and added AuthService to providers list in app.module.ts

--- myclient/src/app/app.module.ts
+++ myclient/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { AuthService } from './services/auth.service';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
